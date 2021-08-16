ac0cb592db55f27cde48783e0c03e9331e985352
Asad Iqbal <asad.iqbal8600@gmail.com>
2021-08-16 12:41:56 +0500

    mongodb configure to live server

--- routes/nftsDB.js
+++ routes/nftsDB.js
@@ -1,13 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGODB_ADDON_URI;
 var dbo;
+var database = "b6qu21qlgsfxjfn";
 
 module.exports = {
 
   insertNftsData: function (nfts, callback) {
     MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
       if (err) throw err;
-      dbo = client.db("NFT_DB");
+      dbo = client.db(database);
       dbo.collection("nfts").insertOne(nfts, function (err, res) {
         if (err) throw err;
         console.log("1 document inserted");
@@ -18,7 +19,7 @@ module.exports = {
   getNftsData: async function () {
 
     let client = await MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    var dbo = client.db("NFT_DB");
+    var dbo = client.db(database);
     let collection = dbo.collection("nfts");
     let res = await collection.find({}).toArray();
     console.log(res);
@@ -27,7 +28,7 @@ module.exports = {
   ,
   getNftData: async function (id) {
     let client = await MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    var dbo = client.db("NFT_DB");
+    var dbo = client.db(database);
     let collection = dbo.collection("nfts");
     let res = await collection.findOne({ info: { $elemMatch: { nftID: id } } });
     console.log(res);
@@ -35,7 +36,7 @@ module.exports = {
   },
   updateNftData: async function (id) {
     let client = await MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
-    var dbo = client.db("NFT_DB");
+    var dbo = client.db(database);
     let collection = dbo.collection("nfts");
     var myquery = { info: { $elemMatch: { nftID: id } } };
     var newvalues = { $set: { name: "Mickey", address: "Canyon 123" } };
